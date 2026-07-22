88464bce5068b978681a7563e4d062d65c193141
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-07-22 10:07:29 +0300

    Implement posthog

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const Home = () => {
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
 
-        <ul className='events'>
+        <ul className='events list-none'>
           {events.map((event) => (
             <li key={event.title}>
               <EventCard {...event} />
